7b90e5eb32a4374c72fa7ffcb30c5410e8abdc23
Carlos Robayo <charlie.roca@hotmail.com>
2017-07-04 16:48:46 -0500

    Init the project

---
+++ app.js
@@ -0,0 +1,9 @@
+var express = require('express');
+
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(8080, function () {
+    console.log('Esperando peticiones en el puerto 8080');
+});
