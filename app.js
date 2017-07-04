ec37c3cc64128d73d082d0f5f303f0a855de3539
Carlos Robayo <charlie.roca@hotmail.com>
2017-07-04 16:55:13 -0500

    Add Server Port

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var express = require('express');
 
 var app = express();
 
+const port = process.env.PORT || 8000;
+
 app.use(express.static(__dirname + '/public'));
 
-app.listen(8080, function () {
-    console.log('Esperando peticiones en el puerto 8080');
-});
+app.listen(port);
