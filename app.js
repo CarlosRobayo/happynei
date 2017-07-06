03cada4b522b0aeab82d504e798f9407316ba430
Carlos Robayo <charlie.roca@hotmail.com>
2017-07-05 22:18:58 -0500

    Advance II Project

--- app.js
+++ app.js
@@ -6,4 +6,14 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
+app.get('*', function (req, res) {
+
+    if (req.url == '/') {
+        res.sendFile('index.html');
+    } else {
+        res.sendFile(__dirname + '/public/views/hpbd.html');
+    }
+
+});
+
 app.listen(port);
